f082b050e6eb87c345ddad3e6a3478cec084f757
Hobick <slovak.kostantin@gmail.com>
2022-11-29 18:58:16 +0200

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -14,9 +14,12 @@ def transform(point):
     pointy = (point[0] - 480)*s[0][1] + (point[1] - 480)*s[1][1]
     return (int(pointx) + 480, int(pointy) + 480)
 
+f1 = open("DS5_transformed_with_aphine.txt", 'w')
+
 for line in f:
     x = int(line.split()[0])
     y = int(line.split()[1])
+    f1.write(str(transform((x, y))[0]) + ' ' + str(transform((x, y))[1]) + '\n')
     img.putpixel(transform((y, 960-x)), (0, 0, 255))
 
 img.show()
